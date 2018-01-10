4f1562c200505e430e7fc6480d1ee391f5bb802b
renzosempai <30874787+renzosempai@users.noreply.github.com>
2018-01-10 08:01:00 +0800

    Major Revision

--- Assets/Scripts/User Interface/NSUtoReg.cs
+++
@@ -1,9 +0,0 @@
- using UnityEngine; 
- using System.Collections;
-
-public class NSUtoReg : MonoBehaviour { 
-    public void ChangeScene(string sceneName) 
-    { 
-        Application.LoadLevel("Registration"); 
-        } 
-    }
--- Assets/Scripts/User Interface/RegToNSU.cs
+++
@@ -1,9 +0,0 @@
- using UnityEngine; 
- using System.Collections;
-
-public class RegToNSU : MonoBehaviour { 
-    public void ChangeScene(string sceneName) 
-    { 
-        Application.LoadLevel("Login"); 
-        } 
-    }
