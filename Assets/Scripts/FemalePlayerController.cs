e811a7ccb496fc9858f29273a1a0b29363e2b94b
renzosempai <30874787+renzosempai@users.noreply.github.com>
2018-02-16 01:27:04 +0800

    Added Female Level 1 Stage
    but got minor bug

--- Assets/Scripts/FemalePlayerController.cs
+++ Assets/Scripts/FemalePlayerController.cs
@@ -7,6 +7,7 @@ public class FemalePlayerController : MonoBehaviour {
 	public float moveSpeed;
 
 	private Animator anim;
+	private Rigidbody2D femaleRigidbody;
 
 	private bool playerMoving;
 	private Vector2 lastMove;
@@ -14,25 +15,38 @@ public class FemalePlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
+		femaleRigidbody = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 		playerMoving = false;
 
 		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f ) 
 		{
-			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			//transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			femaleRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") *moveSpeed, femaleRigidbody.velocity.y); 
 			playerMoving = true;
 			lastMove = new Vector2 (Input.GetAxisRaw ("Horizontal"), 0f);
 		}
 
 		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f ) 
 		{
-			transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			//transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			femaleRigidbody.velocity = new Vector2(femaleRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
 			playerMoving = true;
 			lastMove = new Vector2 (0f, Input.GetAxisRaw ("Vertical"));
+		}
+
+		if (Input.GetAxisRaw ("Horizontal") < 0.5f && Input.GetAxisRaw ("Horizontal") > -0.5f) 
+		{
+			femaleRigidbody.velocity = new Vector2 (0f, femaleRigidbody.velocity.y);
+		}
+
+		if (Input.GetAxisRaw ("Vertical") < 0.5f && Input.GetAxisRaw ("Vertical") > -0.5f) 
+		{
+			femaleRigidbody.velocity = new Vector2 (femaleRigidbody.velocity.x, 0f);
 		}
 
 		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
