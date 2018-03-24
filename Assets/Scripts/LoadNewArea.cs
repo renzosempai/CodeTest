a189f452877eed6ea1cdd5a1be8dfb7e8d75f6f6
renzosempai <30874787+renzosempai@users.noreply.github.com>
2018-03-24 22:32:21 +0800

    Ubay2 pud biya

---
+++ Assets/Scripts/LoadNewArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadNewArea : MonoBehaviour {
+
+	public string levelToLoad;
+
+	public Image black;
+	public Animator Anim;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.name == "Male Player") {
+			StartCoroutine (Fading ());
+		}
+	}
+
+	IEnumerator Fading(){
+		Anim.SetBool ("Fade", true);
+		yield return new WaitUntil (() => black.color.a == 1);
+		SceneManager.LoadScene (levelToLoad);
+	}
+}
